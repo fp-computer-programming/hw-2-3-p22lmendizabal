04402173c9f4331641d5cbea24e12e415cb81c18
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-09-23 10:43:17 -0400

    add file
    hw2-3-3

---
+++ hw2-3-3.py
@@ -0,0 +1,10 @@
+#author: LM (AMDG) 09/23/2021
+# Write a Python program that will display the area and perimeter of a rectangle that has a width of `13.9` and a height of `8.2`
+w = 13.9
+h = 8.2
+
+area = w * h
+
+perimeter = 2 * w + 2 * h
+
+print(" THe area of the rectangle is " + str(area) + ". The perimeter of the rectangle is " + str(perimeter) + ".")
