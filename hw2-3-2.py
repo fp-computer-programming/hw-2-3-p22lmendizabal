91c9fb101df46f472ced148829d4e69000907c05
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-09-23 10:35:57 -0400

    add file
    hw2-3-2

---
+++ hw2-3-2.py
@@ -0,0 +1,13 @@
+# author: LM (AMDG) 09/23/2021
+# area and perimeter of a circle with a radius of 4.6
+#area = pi radius squared
+# perimeter is 2 pi r
+
+r= 4.6
+pi = 3.14
+
+area = pi * r**2
+
+perimeter = 2 * pi * r
+
+print (" The area of the circle is " + str(area) + ". The perimeter of the circle is " + str(perimeter) + ".")
